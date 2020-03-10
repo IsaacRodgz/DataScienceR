97910501a07cd309393fe9c38d1e54f0a9643512
isaacRodgz <chack.rb@gmail.com>
2020-03-10 11:40:49 -0600

    ADD k-means

--- tarea3.R
+++ tarea3.R
@@ -1,6 +1,8 @@
 library(MVA)
 library(mclust)
 
+# Load data
+
 data("USairpollution")
 usair.data <- USairpollution[-1]
 
@@ -14,13 +16,15 @@ col[c(7,30)] <- "red"
 pairs(usair.data, lower.panel = NULL, main="US air pollution", pch=pch, col=col)
 
 usair.data <- subset(usair.data, manu < 1500)
+usair.data <- scale(usair.data ,center=FALSE, scale=TRUE)
+
+# EM clustering
 
 mc <- Mclust(usair.data)
 summary(mc)
 groups <- mc$classification
 
 pr <- prcomp(usair.data)$x[, 1:2]
-
 plot(pr)
 text(pr, labels=groups, cex=0.75, pos=2, col="red")
 
@@ -39,7 +43,35 @@ boxplot(subset(USairpollution, manu < 1500)$SO2~groups,
         border="black"
 )
 
+# Hierarchical clustering
+
 dm <- dist(usair.data)
 plot(cs <- hclust(dm, method = "single"))
-plot(cs <- hclust(dm, method = "complete"))
-plot(cs <- hclust(dm, method = "average"))
+plot(ca <- hclust(dm, method = "average"))
+plot(cc <- hclust(dm, method = "complete"))
+
+usair_pc <- princomp(dm, cor = TRUE)
+xlim <- range(usair_pc$scores[,1])
+plot(usair_pc$scores[,1:2], type = "n", xlim = xlim, ylim = xlim)
+lab <- cutree(cc, h = 1.6)
+text(usair_pc$scores[,1:2], labels = lab, cex = 0.6)
+
+# K-means
+
+rge <- sapply(usair.data, function(x) diff(range(x)))
+usair.data_s <- sweep(usair.data, 2, rge, FUN = "/")
+
+n <- nrow(usair.data_s)
+wss <- rep(0, 6)
+wss[1] <- (n-1)*sum(sapply(usair.data_s, var))
+
+for (i in 2:6)
+  wss[i] <- sum(kmeans(usair.data_s, centers=i)$withinss)
+
+plot(1:6, wss, type = "b", xlab = "Number of groups", ylab = "Within groups sum of squares")
+
+km <- kmeans(usair.data_s, centers=3)
+
+pr <- prcomp(usair.data_s)$x[, 1:2]
+plot(pr)
+text(pr, labels=km$cluster, cex=0.75, pos=2, col="red")
