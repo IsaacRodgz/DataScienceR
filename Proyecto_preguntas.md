323b828f4bf3280fe10238af7c7e315d42f2c61c
IsaacRodgz <chack.rb@gmail.com>
2020-05-21 20:26:13 -0500

    EDA for SARS-CoV-2 dataset

---
+++ Proyecto_preguntas.md
@@ -0,0 +1,15 @@
+## Isaac Rodríguez Bribiesca
+
+### Preguntas a responder usando base COVID-19:
+
+1. Qué entidades son las que más casos de infectados han registrado?
+2. ¿Qué variables ayudan más a predecir si un paciente va a fallecer o no?
+3. Dadas ciertas comorbilidades, ¿Qué probabilidad se tiene de que el paciente fallezca?
+4. Dadas ciertas comorbilidades, ¿Qué probabilidad se tiene de que el paciente sea intubado?
+5. ¿Qué tipo de comorbilidades son más frecuentes es pacientes fallecidos?
+6. ¿Cuál es la distribución de edades de las personas registradas?
+7. ¿Qué variables son las que concentran mayor variabilidad de los datos?
+8. ¿Qué tipo de variables tienes un comportamiento similar?
+9. ¿Cómo se agrupan los síntomas en las personas fallecidas?
+10. ¿Se puede hacer una clasificación de personas que diga si tiene "bajo riesgo", "medio riesgo" y "alto riesgo" de fallecer?
+11. ¿Qué tanto influye el tiempo que pasa desde que el paciente empezó a tener síntomas hasta que es ingresado al hospital, para estimar el riesgo de que la persona fallezca?
--- tarea6/tarea6_wine.R
+++ tarea6/tarea6_wine.R
@@ -2,12 +2,22 @@ library(tidymodels)
 library(tidyverse)
 library(magrittr)
 library(rpart.plot)
+library(ggcorrplot)
+library(gridExtra)
+library(ica)
+library(fastICA)
+library(rsample)
+library(caret)
+library(discrim)
+library(klaR)
 
 set.seed(101)
 
+# Análisis datos 1: Vino.
+
 # Reading and Preprocessing
 
-#setwd("~/Documents/CIMAT/DataScienceR/tarea6/")
+setwd("~/Documents/CIMAT/DataScienceR/tarea6/")
 wine.data.train = read_tsv("wine.train.txt")
 wine.data.test = read_tsv("wine.test.txt")
 
@@ -114,10 +124,20 @@ wine.lr.model
 # Define Model - Multi Layer Perceptron
 # -------------------------------------
 
+wine.recipe = recipe(classdigit ~ ., data = wine.data.train) %>%
+  step_center(all_predictors(), -all_outcomes()) %>%
+  step_scale(all_predictors(), -all_outcomes()) %>%
+  prep()
+
+wine.train = juice(wine.recipe)
+wine.test = wine.recipe %>%
+  bake(wine.data.test)
+
+
 wine.mlp = mlp(mode = "classification",
                activation = "relu",
                hidden_units = tune(),
-               dropout = 0.4,
+               dropout = 0.0,
                epochs = 10) %>% 
   set_engine("nnet")
 
@@ -172,10 +192,10 @@ model.tuned %>%
 
 wine.lr.model = mlp(mode = "classification",
                     activation = "relu",
-                    hidden_units = 2,
-                    dropout = 0,
+                    hidden_units = 1,
+                    dropout = 0.,
                     epochs = 10) %>% 
-  set_engine("keras") %>% 
+  set_engine("nnet") %>% 
   fit(classdigit ~ ., data = wine.train)
 
 # Evaluation metrics
@@ -184,3 +204,144 @@ wine.lr.model %>%
   predict(wine.test) %>%
   bind_cols(wine.test) %>%
   metrics(truth = classdigit, estimate = .pred_class)
+
+# ----------------------
+# Análisis datos 2: Spam.
+# ----------------------
+
+spam.data = read_csv("spambase.data", col_names = FALSE)
+spam.data <- spam.data %>% rename(class = X58)
+spam.data %<>% mutate(class = as.factor(class))
+log_m <- function(x) log(x+0.001)
+spam.data %<>% mutate_if(is.numeric, log1p)
+
+# EDA
+
+# Correlation plot 
+
+# Visualize correlation plot
+
+corr=round(cor(spam.data[-58]), 1)
+
+ggcorrplot(corr, hc.order = TRUE, 
+           type = "lower", 
+           lab = TRUE, 
+           lab_size = 3, 
+           method="circle", 
+           colors = c("tomato2", "white", "springgreen3"), 
+           title="Correlation of wine variables")
+
+# Distribution
+
+p1 <- ggplot(spam.data, aes(x=X1)) +
+  geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+                 binwidth=.1,
+                 colour="black", fill="white") +
+  geom_density(alpha=.2, fill="#FF6666") + labs(x = "word_freq_make")
+
+p2 <- ggplot(spam.data, aes(x=X10)) +
+  geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+                 binwidth=.2,
+                 colour="black", fill="white") +
+  geom_density(alpha=.2, fill="#FF6666") + labs(x = "word_freq_mail")
+
+p3 <- ggplot(spam.data, aes(x=X20)) +
+  geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+                 binwidth=.2,
+                 colour="black", fill="white") +
+  geom_density(alpha=.2, fill="#FF6666") + labs(x = "word_freq_credit")
+
+p4 <- ggplot(spam.data, aes(x=X55)) +
+  geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+                 binwidth=0.3,
+                 colour="black", fill="white") +
+  geom_density(alpha=.2, fill="#FF6666") + labs(x = "capital_run_length_average")
+
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+# PCA
+
+pr = prcomp(spam.data[-58])
+plot(pr, type = "l")
+plot(cumsum(pr$sdev[1:5]^2)/sum(pr$sdev^2), xlab = "Num of PC", ylab = "Explained variance", type = "b")
+summary(pr)
+
+# Data PCA
+spam.pca <- as.data.frame(predict(pr, newdata = spam.data))
+spam.pca <- cbind(spam.pca[,c(1,2)], spam.data$class)
+names(spam.pca)[3] <- "class"
+
+spam.data_s <- scale(spam.data[,-58])
+pr = princomp(spam.data_s)
+pairs(pr$scores[,1:3], col=rainbow(2)[pull(spam.data, class)], asp=1)
+
+# ---------
+# Modelo LR
+# ---------
+
+# Train - Test split
+spam.split <- createDataPartition(spam.data$class, p = .8, 
+                                                list = FALSE, 
+                                                times = 1)
+spam.train <- spam.data[as.vector(spam.split),]
+spam.test <- spam.data[-as.vector(spam.split),]
+
+spam.recipe = recipe(class ~ ., data = spam.train) %>%
+  #step_log(all_predictors(), offset = 1) %>%
+  prep()
+
+spam.train.r = bake(spam.recipe, spam.train)
+spam.test.r = bake(spam.recipe, spam.test)
+
+spam.lr.model = logistic_reg(mode = "classification") %>%
+  set_engine("glm") %>% 
+  fit(class ~ ., data = spam.train.r)
+
+# Evaluation metrics
+
+spam.lr.model %>%
+  predict(spam.test.r) %>%
+  bind_cols(spam.test.r) %>%
+  metrics(truth = class, estimate = .pred_class)
+
+# Analyze results
+
+with(spam.lr.model$fit, null.deviance - deviance)
+with(spam.lr.model$fit, df.null - df.residual)
+with(spam.lr.model$fit, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE))
+
+# ---------
+# Modelo Naive Bayes
+# ---------
+
+# Train - Test split
+spam.split <- createDataPartition(spam.data$class, p = .8, 
+                                  list = FALSE, 
+                                  times = 1)
+spam.train <- spam.data[as.vector(spam.split),]
+spam.test <- spam.data[-as.vector(spam.split),]
+
+spam.recipe = recipe(class ~ ., data = spam.train) %>%
+  step_factor2string(all_outcomes()) %>%
+  step_zv(all_predictors()) %>%
+  step_log(all_predictors(), offset = 1) %>%
+  prep()
+
+spam.train.r = bake(spam.recipe, spam.train)
+spam.test.r = bake(spam.recipe, spam.test)
+
+spam.nb.model <- naive_Bayes(mode = "classification") %>%
+  set_engine("klaR") %>%
+  fit(class ~ ., data = spam.train.r)
+  #fit_xy(x = spam.train.r[,-58], y = spam.train.r[,58])
+
+#naive_Bayes(smoothness = .8) %>%
+#  set_engine("klaR") %>%
+#  translate()
+
+# Evaluation metrics
+
+spam.nb.model %>%
+  predict(spam.test.r) %>%
+  bind_cols(spam.test.r) %>%
+  metrics(truth = class, estimate = .pred_class)
